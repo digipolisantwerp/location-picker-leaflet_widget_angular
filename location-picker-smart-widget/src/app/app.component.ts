65c3602046f2974b80f14c3c2078c7132a27f9ea
jeroen brock <jeroen.brock@endare.com>
2018-05-25 10:43:58 +0200

    cleaned sandbox component

--- location-picker-smart-widget/src/app/app.component.ts
+++ location-picker-smart-widget/src/app/app.component.ts
@@ -1,28 +1,28 @@
 import { Component } from '@angular/core';
-import { baseMapAntwerp, baseMapWorldGray, LeafletLayer, LeafletMap } from '@acpaas-ui/leaflet';
-import { LocationPickerValue } from '@acpaas-ui-widgets/ngx-location-picker';
+import { baseMapWorldGray, LeafletLayer, LeafletMap } from '@acpaas-ui/leaflet';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
 
-    layer:LeafletLayer =baseMapWorldGray;
-    picker: LocationPickerValue;
-    url:string='http://localhost:9999/api/locations';
+    layer: LeafletLayer = baseMapWorldGray;
+    url: string = 'http://localhost:9999/api/locations';
     leafletMap: LeafletMap = new LeafletMap({
         zoom: 13, // default zoom level
         center: [51.215, 4.425], // default center point
-        onAddPolygon: (layer) => {},
-        onAddLine: (layer) => {},
-        onEditFeature: (feature) => {},
+        onAddPolygon: (layer) => {
+        },
+        onAddLine: (layer) => {
+        },
+        onEditFeature: (feature) => {
+        }
     });
 
-    addressResolvedCallback= (location) =>{
+    addressResolvedCallback = (location) => {
         console.log(location);
-    }
+    };
 
 }
