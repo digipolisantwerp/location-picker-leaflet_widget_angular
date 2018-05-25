298788d6d67f672cb74a8f653e00e2d9667b7e55
jeroen brock <jeroen.brock@endare.com>
2018-05-25 10:44:17 +0200

    imported location service in sandbox app

--- location-picker-smart-widget/src/app/app.module.ts
+++ location-picker-smart-widget/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { LocationPickerLeafletComponent } from './modules/location-picker-leaflet/location-picker-leaflet.component';
 import { LocationPickerModule } from '@acpaas-ui-widgets/ngx-location-picker';
 import { LeafletModule } from '@acpaas-ui/leaflet';
+import { LocationPickerLeafletService } from './modules/location-picker-leaflet/location-picker-leaflet.service';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,7 @@ import { LeafletModule } from '@acpaas-ui/leaflet';
         LeafletModule,
         LocationPickerModule
     ],
-    providers: [],
+    providers: [LocationPickerLeafletService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
