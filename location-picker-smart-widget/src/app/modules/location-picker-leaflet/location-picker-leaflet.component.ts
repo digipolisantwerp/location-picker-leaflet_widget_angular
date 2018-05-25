37b0218e28cbd4322b1da99c1f79e7e6746c3069
jeroen brock <jeroen.brock@endare.com>
2018-05-25 10:42:14 +0200

    added validators and comments in the component.ts

--- location-picker-smart-widget/src/app/modules/location-picker-leaflet/location-picker-leaflet.component.ts
+++ location-picker-smart-widget/src/app/modules/location-picker-leaflet/location-picker-leaflet.component.ts
@@ -14,41 +14,64 @@ L.Icon.Default.mergeOptions({
 
 
 @Component({
-    selector: 'app-location-picker-leaflet',
+    selector: 'aui-location-leaflet-smart-widget',
     templateUrl: './location-picker-leaflet.component.html',
     styleUrls: ['./location-picker-leaflet.component.css']
 })
 export class LocationPickerLeafletComponent implements OnInit {
 
-    @Input() locationPicker: LocationPickerValue;
-    @Input() url: string;
+    private locationPicker: LocationPickerValue;
+
+    @Input() locationPickerUrl: string;
     @Input() leafletMap: LeafletMap;
     @Input() layer: LeafletLayer;
-    @Input() markerIconPath;
     @Output() addressResolvedCallback: EventEmitter<LocationPickerValue> = new EventEmitter<LocationPickerValue>();
 
 
     private marker: L.marker;
 
-    constructor() {
+    constructor(private locationPickerLeafletService:LocationPickerLeafletService) {
     }
 
     ngOnInit() {
 
 
+
+        // Checks  the required attributes
+        if(!this.locationPickerUrl) throw new Error("Attribute 'locationPickerUrl' is required on aui-location-leaflet-smart-widget element.");
+        if(!this.leafletMap) throw new Error("Attribute 'leafletMap' is required on aui-location-leaflet-smart-widget element.");
+        if(!this.layer) throw new Error("Attribute 'layer' is required on aui-location-leaflet-smart-widget element.");
+
+        // Set the server url in the service
+        this.locationPickerLeafletService.setUrl(this.locationPickerUrl);
+
+        // Layer can only be drawn on the leaflet after it has been initted.
         this.leafletMap.onInit.subscribe(() => {
 
 
+            // Adding the layer to the leaflet.
             this.leafletMap.addTileLayer(this.layer);
+
+            // Declare a marker with standard icon, widget can be expanded with custom icon support.
             this.marker = new L.marker(this.leafletMap.map.getCenter());
+
+            //Add marker to the leaflet.
             this.marker.addTo(this.leafletMap.map);
 
+            // Subscribe on the map move event. will trigger each time user moves the app.
             this.leafletMap.map.on('move', () => {
+
                 // when the map moves, marker should be set at the center
                 this.marker.setLatLng(this.leafletMap.map.getCenter());
 
             });
+
+            // Subscribe to the dragend event. This will only trigger when the user stopped moving the map.
+            // Using this event to prevent continuous calls
             this.leafletMap.map.on('dragend', () => {
+
+                //Calling the server to get location from coordinates
+                this.locationPickerLeafletService.getLocationFromCoordinates(this.leafletMap.map.getCenter());
                 console.log(this.leafletMap.map.getCenter());
 
             });
