c67eaef7be1f443713a720c0c929b8c071e337db
Lieven Van Gestel <lieven.vangestel@digipolis.be>
2019-02-26 14:02:57 +0100

    Correct service unit tests

--- src/location-picker-leaflet/location-picker-leaflet.service.spec.ts
+++ src/location-picker-leaflet/location-picker-leaflet.service.spec.ts
@@ -1,42 +1,39 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-import { Observable } from 'rxjs';
-import { of } from 'rxjs';
-
 import { LocationPickerLeafletService } from './location-picker-leaflet.service';
-import {
-  LocationItem,
-  Coordinates,
-  LocationType,
-} from './LocationItem.domain';
 
 const mockCoordinates = { lat: 51.19506875061893, lng: 4.381795173474985 };
-
 const mockId = '2001887628';
 
 const mockResponse1 = {
-  id: 'P_DA/Locaties/MapServer/18/1168460',
-  name: 'Burgemeester De Boeylaan (Deurne)',
-  layer: 'straatnaam',
-  locationType: LocationType.Street,
-  coordinates: {
-    latLng: { lat: 51.21025180508141, lng: 4.474143732169805 },
-  },
-  street: 'Burgemeester De Boeylaan'
-
+  location: {
+    id: '2329',
+    name: 'Ploegstraat 15',
+    street: 'Ploegstraat',
+    number: '15',
+    postal: '2018',
+    district: 'Antwerpen',
+    locationType: 'street',
+    coordinates: {
+      latLng: {lat: 51.21492224512688, lng: 4.42519775286003}
+    }
+  }
 };
 
 const mockResponse2 = [{
-  id: 'P_DA/Locaties/MapServer/18/1168460',
-  name: 'Burgemeester De Boeylaan (Deurne)',
-  layer: 'straatnaam',
-  locationType: LocationType.Street,
+  id: mockId,
+  name: 'Generaal Armstrongweg 1, 2020 Antwerpen',
+  street: 'Generaal Armstrongweg',
+  number: '1',
+  postal: '2020',
+  district: 'Antwerpen',
+  locationType: 'number',
+  layer: 'CRAB',
   coordinates: {
-    latLng: { lat: 51.21025180508141, lng: 4.474143732169805 },
-  },
-  street: 'Burgemeester De Boeylaan'
-
+    latLng: mockCoordinates,
+    lambert: { x: 150910.2899999991, y: 209456.6099999994 }
+  }
 }];
 
 describe('Location Picker Leaflet Service', () => {
@@ -60,7 +57,7 @@ describe('Location Picker Leaflet Service', () => {
   it('should return location when props are coordinates', () => {
     locationPickerLeafletService.getLocation('/mockUrl', mockCoordinates)
       .then((result: any) => {
-        expect(result).toEqual(mockResponse1);
+        expect(result).toEqual(mockResponse1.location);
       });
 
     const req = httpMock.expectOne(request => request.method === 'GET' && request.url === '/mockUrl');
@@ -70,7 +67,7 @@ describe('Location Picker Leaflet Service', () => {
   it('should return location when prop is location id', () => {
     locationPickerLeafletService.getLocation('/mockUrl', mockId)
       .then((result: any) => {
-        expect(result).toEqual(mockResponse1);
+        expect(result).toEqual(mockResponse2[0]);
       });
 
     const req = httpMock.expectOne(request => request.method === 'GET' && request.url === '/mockUrl');
